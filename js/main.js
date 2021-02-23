7b13a74d18fbfcb495e4d76d67d0e2a7ebdfb85f
James Roach <roachjweston@gmail.com>
2021-02-23 23:00:36 +0000

    changed class name of front page to hidden whenever one of the buttons is clicked

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
+var $front = document.querySelector('#front-page');
+
 function categorySelect(event) {
   if (event.target.className === 'chars' || event.target.getAttribute('id') === 'chars') {
-    event.target.className = 'chars';
+    $front.className = 'hidden';
   } else if (event.target.className === 'locations' || event.target.getAttribute('id') === 'locations') {
-    event.target.className = 'locations';
+    $front.className = 'hidden';
   } else if (event.target.className === 'episodes' || event.target.getAttribute('id') === 'episodes') {
-    event.target.className = 'episodes';
+    $front.className = 'hidden';
   } else if (event.target.className === 'favs' || event.target.getAttribute('id') === 'favs') {
-    event.target.className = 'favs';
+    $front.className = 'hidden';
   }
 }
 
