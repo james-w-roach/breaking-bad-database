37c0ba0635ecadfca0e6395a26392c588be1c6cd
James Roach <roachjweston@gmail.com>
2021-02-23 23:46:26 +0000

    specific ajax request function is called depending on which button is clicked

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 var $front = document.querySelector('#front-page');
+var $ajaxList = document.querySelector('#ajax-list');
 var $ul = document.querySelector('ul');
 
 function categorySelect(event) {
   if (event.target.className === 'chars' || event.target.getAttribute('id') === 'chars') {
     $front.className = 'hidden';
+    $ajaxList.className = 'ajax-list';
+    var char = 'character';
+    getAPIData(char);
   } else if (event.target.className === 'locations' || event.target.getAttribute('id') === 'locations') {
     $front.className = 'hidden';
+    $ajaxList.className = 'ajax-list';
+    var location = 'location';
+    getAPIData(location);
   } else if (event.target.className === 'episodes' || event.target.getAttribute('id') === 'episodes') {
     $front.className = 'hidden';
+    $ajaxList.className = 'ajax-list';
+    var episode = 'episode';
+    getAPIData(episode);
   } else if (event.target.className === 'favs' || event.target.getAttribute('id') === 'favs') {
     $front.className = 'hidden';
   }
@@ -15,16 +25,20 @@ function categorySelect(event) {
 
 window.addEventListener('click', categorySelect);
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://rickandmortyapi.com/api/character');
-xhr.responseType = 'json';
+var xhr;
 
-xhr.addEventListener('load', function () {
-  loadDOM();
-  window.addEventListener('DOMContentLoaded', loadDOM);
-});
+function getAPIData(category) {
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://rickandmortyapi.com/api/' + category);
+  xhr.responseType = 'json';
 
-xhr.send();
+  xhr.addEventListener('load', function () {
+    loadDOM();
+    window.addEventListener('DOMContentLoaded', loadDOM);
+  });
+
+  xhr.send();
+}
 
 function loadDOM(event) {
   for (var i = 0; i < xhr.response.results.length; i++) {
@@ -37,14 +51,16 @@ function createDOM(object) {
   var $li = document.createElement('li');
   $li.className = 'list-item';
 
-  var $column25 = document.createElement('div');
-  $column25.className = 'column-25';
-  $li.appendChild($column25);
+  if (object.image) {
+    var $column25 = document.createElement('div');
+    $column25.className = 'column-25';
+    $li.appendChild($column25);
 
-  var $img = document.createElement('img');
-  $img.setAttribute('src', object.image);
-  $img.className = 'list-img';
-  $column25.appendChild($img);
+    var $img = document.createElement('img');
+    $img.setAttribute('src', object.image);
+    $img.className = 'list-img';
+    $column25.appendChild($img);
+  }
 
   var $column75 = document.createElement('div');
   $column75.className = 'column-75';
