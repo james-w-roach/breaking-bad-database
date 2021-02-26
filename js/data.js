c28d04da667c2567842284ce05c5270510c36437
James Roach <roachjweston@gmail.com>
2021-02-26 21:08:01 +0000

    data is now saved to local storage when a beforeunload event fires

--- js/data.js
+++ js/data.js
@@ -5,3 +5,16 @@ var data = {
   Locations: [],
   Episodes: []
 };
+
+var localStorageJSON = localStorage.getItem('C-137 Data');
+
+if (localStorageJSON) {
+  data = JSON.parse(localStorageJSON);
+}
+
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('C-137 Data', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
